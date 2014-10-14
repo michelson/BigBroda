7593d3f839e9af5691fdfb7357669a0a306e26f2
Diego Rodriguez <diec123@gmail.com>
2014-10-14 18:04:34 +0200

    require debugger only in ruby1_9 platform

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,6 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
-require 'debugger'
+require 'debugger' if Gem.ruby_version < Gem::Version.new('2.0')
 require File.join(File.dirname(__FILE__), '../lib', 'google_bigquery')
 require 'stringio'
 require "pry"
